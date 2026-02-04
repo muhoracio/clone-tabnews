4688b2678268be2ab72773ecee0de6c49c2377b7
muhoracio <muhoracio@gmail.com>
2026-02-04 13:12:33 -0300

    feat: add `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,51 @@
+import { resolve } from "node:path";
+import database from "infra/database.js";
+import migrationRunner from "node-pg-migrate";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendindMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendindMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+      dbClient,
+    });
+
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendindMigrations,
+  runPendindMigrations,
+};
+
+export default migrator;
