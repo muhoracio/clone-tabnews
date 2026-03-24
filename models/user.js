0f3d452171a1ee95c1abb91bbbe635bbb078eac0
muhoracio <muhoracio@gmail.com>
2026-03-24 13:55:00 -0300

    feat: create `authentication` and `session` models and `/api/v1/sessions` endpoint

--- models/user.js
+++ models/user.js
@@ -33,6 +33,37 @@ async function findOneByUsername(username) {
   }
 }
 
+async function findOneByEmail(email) {
+  const userFound = await runSelectQuery(email);
+  return userFound;
+
+  async function runSelectQuery(email) {
+    const result = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          users
+        WHERE 
+          LOWER(email) = LOWER($1)
+        LIMIT 
+          1
+        ;
+      `,
+      values: [email],
+    });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O email não foi encontrado no sistema.",
+        action: "Verifique se o email está digitado corretamente.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
+
 async function create(userInputValues) {
   await validateUniqueUsername(userInputValues.username);
   await validateUniqueEmail(userInputValues.email);
@@ -175,6 +206,7 @@ async function validatePasswordInObject(userInputValues) {
 const user = {
   create,
   findOneByUsername,
+  findOneByEmail,
   update,
 };
 
