9254a5690c2b7b41cb47b2872970a1220378889d
muhoracio <muhoracio@gmail.com>
2026-05-19 10:02:12 -0300

    refactor: add explicit `return` in the `/api/v1/status` `GET` handler

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -50,5 +50,5 @@ async function getHandler(request, response) {
     statusObject,
   );
 
-  response.status(200).json(secureOutputValues);
+  return response.status(200).json(secureOutputValues);
 }
